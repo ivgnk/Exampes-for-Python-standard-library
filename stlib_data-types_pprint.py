44a9bf8dcffe16ac583129d9568ce26a3c3c812f
Ivan Genik <IVGenik@rambler.ru>
2023-09-07 12:18:22 +0500

    Add files via upload

---
+++ stlib_data-types_pprint.py
@@ -0,0 +1,6 @@
+'''
+pprint — Data pretty printer¶
+https://docs.python.org/3/library/pprint.html
+Красивый вывод списков, словарей и других структур данных
+https://docs-python.ru/standart-library/modul-pprint-python/
+'''
